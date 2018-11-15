0571572bfe5886b37d5df2b9dd887d77fd19b0fc
mahnazzzz <mahnaazi@yahoo.com>
2018-11-15 22:54:31 +0100

    RequestBody is ready

--- src/main/java/com/loanbroker/bank/web/service/resource/RequestQuoteResource.java
+++ src/main/java/com/loanbroker/bank/web/service/resource/RequestQuoteResource.java
@@ -10,6 +10,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Random;
 
-@JsonSerializableSchema
+
 @RestController
 @RequestMapping("quote")
 @Produces(MediaType.APPLICATION_JSON)
@@ -30,23 +31,22 @@ public class RequestQuoteResource {
     @Autowired
     private RabbitTemplate template;
 
+
     @PostMapping
-    public QuoteResponse getQuote(@Valid QuoteRequest quoteRequest) {
+    public QuoteResponse getQuote(@RequestBody QuoteRequest quoteRequest) {
         // Do something with the quote here. Map it to a QuoteResponse object.
-
-    int ssn = quoteRequest.getSsn();
-
+       QuoteRequest q= quoteRequest;
+    int ssn = q.getSsn();
     System.out.print("HER IS :  "+ssn +"\n");
-    int creditScore = quoteRequest.getCreditScore();
-    double loanAmount = quoteRequest.getLoanAmount();
-    int loanDurationMount = quoteRequest.getLoanDurationMount();
+    int creditScore = q.getCreditScore();
+    double loanAmount = q.getLoanAmount();
+    int loanDurationMount = q.getLoanDurationMount();
         System.out.print("HER ISss :  "+loanDurationMount);
     double interstRate ;
     double x = (Math.random() * (10.0));
     double y = (Math.random() * (50.0));
     if (creditScore < 600 && loanAmount < 360) {
         interstRate = 7.5;
-
     } else if (creditScore >= 600 && loanAmount > 360) {
         interstRate = x;
     } else if (creditScore >= 600 && loanAmount < 360) {
@@ -54,12 +54,11 @@ public class RequestQuoteResource {
     } else {
         interstRate = 10.0;
     }
-
         QuoteResponse quoteResponse = new QuoteResponse(interstRate,ssn);
         // Replace uri in the create call with uri from quoteRequest
         // This piece of code sets the factory of the template to the url specified from the request
-      /*  ConnectionFactory factory = ConnectionFactoryBuilder.create("amqp://guest:guest@localhost:5672/");
-        template.setConnectionFactory(factory);
+       ConnectionFactory factory = ConnectionFactoryBuilder.create("amqp://guest:guest@localhost:5672/");
+        /*template.setConnectionFactory(factory);
         template.convertAndSend(quoteResponse);*/
         return quoteResponse;
     }
